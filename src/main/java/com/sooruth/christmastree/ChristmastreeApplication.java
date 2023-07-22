7a6e8b6907dbf2bb8ade912046e401f436be6e5e
zulfekar <17052721+ZS25@users.noreply.github.com>
2023-07-22 12:55:36 -0400

    Created the trunk

--- src/main/java/com/sooruth/christmastree/ChristmastreeApplication.java
+++ src/main/java/com/sooruth/christmastree/ChristmastreeApplication.java
@@ -1,6 +1,7 @@
 package com.sooruth.christmastree;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -8,31 +9,41 @@ public class ChristmastreeApplication {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of lines of the Christmas tree: ");
-        int numberOfLines = scanner.nextInt();
-        numberOfLines = numberOfLines *2;
+        System.out.print("Enter the number of branches of the Christmas tree: ");
+        int numberOfLinesEntered = scanner.nextInt();
+        int numberOfLines = numberOfLinesEntered * 2;
+
 
         int lines = numberOfLines;
-        for(int line = 0; line < lines; line++){
-            int spaces = lines -1;
+        for (int line = 0; line < lines; line++) {
+            int spaces = lines - 1;
 
-            for(int space = 0; space < spaces; space++){
+            for (int space = 0; space < spaces; space++) {
                 System.out.print(" ");
             }
 
-            int stars = numberOfLines-spaces;
-            for (int star = 0; star < stars; star++){
+            int stars = numberOfLines - spaces;
+            for (int star = 0; star < stars; star++) {
                 System.out.print("*");
             }
 
-            stars = numberOfLines-spaces-1;
-            for (int star = 0; star < stars; star++){
+            stars = numberOfLines - spaces - 1;
+            for (int star = 0; star < stars; star++) {
                 System.out.print("*");
             }
 
             System.out.println();
             lines--;
+
+            //trunk
+            if (lines == numberOfLinesEntered) {
+                int spacesFinal = numberOfLines - 1;
+                for (int space = 0; space < spacesFinal; space++) {
+                    System.out.print(" ");
+                }
+                System.out.println("^");
+
+            }
         }
     }
-
 }
